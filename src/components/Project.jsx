aa6d84c98dc55bbf00b1ff7768bbc6f3f2a8b822
RutamBhagat <rutambhagat@gmail.com>
2022-11-30 14:56:25 +0530

    add demo links

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,16 +10,18 @@ const Project = () => {
         "FastKitchen has a simple UI which allows its users to quickly and conveniently order food. Recentonline pizza delivery apps are trending among users as these services are time-saving and easy to use. This app has a fully responsive layout. It allows customers to pick from different types of pizza from your restaurant and with the pizza constructor feature they can customize their pizza by changing flavors, size, toppings etc. according to their taste, and place an order. And lastly the app was deployed on Netlify.",
       github_link: "https://github.com/RutamBhagat/pizza_UI_react",
       live_link: "https://rutambhagatgithubiopizzauireact.netlify.app/",
+      demo_link: "https://youtu.be/jr-RZTDfxHs",
     },
     {
       img: "https://lh3.googleusercontent.com/Kx3hndq4t-0_Ib-_2Lb3yi2e2mjId2sBPg08nFA-MOWfwTOUCXpivJWeWgEZREjL7sM=h500",
       name: "SmartBrain: Face detection app",
       technologies:
-        "HTML, CSS, Tailwind, Javascript, React, Node, Express, PostgreSQL",
+      "HTML, CSS, Tailwind, Javascript, React, Node, Express, PostgreSQL",
       description:
-        "When you enter an image URI in the input and press detect, It'll detect all the faces in the image using an image recognition API, and put boxes around them. In frontend it uses ReactJS In backend it has its own RESTful API server with express. It stores user information such as username and also hash passwords securely in a PostgreSQL database. SmartBrain V-1 is a fully responsive web site, and the react-tsparticles make it look even cooler. And lastly the app was deployed on Heroku. ",
+      "When you enter an image URI in the input and press detect, It'll detect all the faces in the image using an image recognition API, and put boxes around them. In frontend it uses ReactJS In backend it has its own RESTful API server with express. It stores user information such as username and also hash passwords securely in a PostgreSQL database. SmartBrain V-1 is a fully responsive web site, and the react-tsparticles make it look even cooler. And lastly the app was deployed on Heroku. ",
       github_link: "https://github.com/RutamBhagat/SmartBrain_V-2",
       live_link: "https://smart-brain-v-2.herokuapp.com",
+      demo_link: "https://youtu.be/7Y_uCxnZuOg",
     },
   ];
   return (
@@ -47,8 +49,17 @@ const Project = () => {
                     {project.technologies}
                   </p>
                   <p className="">{project.description}</p>
-                  <a href="#about">
-                    <div className="mt-8 mr-4 inline-block bg-gradient-to-r from-cyan-200 to-cyan-600 p-1">
+                  <a href={project.demo_link}>
+                    <div className="mt-8 inline-block bg-gradient-to-r from-cyan-200 to-cyan-600 p-1">
+                      <div className="parent bg-gray-900 hover:bg-gradient-to-r hover:from-cyan-200 hover:to-cyan-600">
+                        <button className="parent-hover:text-gray z-5 bg-gradient-to-r from-cyan-200 to-cyan-600 bg-clip-text p-2 font-bold text-transparent">
+                          Demo
+                        </button>
+                      </div>
+                    </div>
+                  </a>
+                  <a href={project.live_link}>
+                    <div className="mt-8 mx-3 inline-block bg-gradient-to-r from-cyan-200 to-cyan-600 p-1">
                       <div className="parent bg-gray-900 hover:bg-gradient-to-r hover:from-cyan-200 hover:to-cyan-600">
                         <button className="parent-hover:text-gray z-5 bg-gradient-to-r from-cyan-200 to-cyan-600 bg-clip-text p-2 font-bold text-transparent">
                           See Live
@@ -56,7 +67,7 @@ const Project = () => {
                       </div>
                     </div>
                   </a>
-                  <a href="#about">
+                  <a href={project.github_link}>
                     <button className="z-5 bg-gradient-to-r from-cyan-200 to-cyan-600 bg-clip-text p-2 font-bold text-transparent">
                       Source Code
                     </button>
